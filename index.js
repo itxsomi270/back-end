9b3a896f5c8c3ba193883a0feb07346be9db6651
Usama Madni <usama.madni@tecklogics.com>
2024-04-23 19:56:47 +0500

    mongo added to the server

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.post('/posts', (req, res) => {
+mongoose.connect('mongodb://127.0.0.1:27017/posts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(error => {
+  console.error('MongoDB connection error:', error);
+});
 
+const postSchema = mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+app.post('/posts', async (req, res) => {
   try {
-    let postDataArray = [];
-    try {
-      const existingData = fs.readFileSync('data.json', 'utf8');
-      postDataArray = JSON.parse(existingData);
-    } catch (err) {
-      if (err.code !== 'ENOENT') {
-        console.error('Error reading file:', err.message);
-      }
-    }
-    
     const postData = req.body;
     console.log('Received post data:', postData);
-    postDataArray.push(postData);
+    
+    const newPost = new Post(postData);
+    await newPost.save();
 
-    fs.writeFile('data.json', JSON.stringify(postDataArray), (err) => {
-      if (err) {
-        console.error('Error writing to file:', err.message);
-        res.status(500).json({ error: 'Internal server error' });
-        return;
-      }
-      console.log('Data saved to data.json');
-      res.status(200).json({ message: 'Request received successfully' });
-    });
+    console.log('Data saved to MongoDB');
+    res.status(200).json({ message: 'Post created successfully' });
   } catch (error) {
     console.error('Error processing request:', error.message);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.get('/posts', (req, res) => {
-
+app.get('/posts', async (req, res) => {
   try {
-    const existingData = fs.readFileSync('data.json', 'utf8');
-    const postDataArray = JSON.parse(existingData);
+    const postDataArray = await Post.find({});
     res.status(200).json(postDataArray);
   } catch (error) {
-    console.error('Error reading file:', error.message);
+    console.error('Error reading from MongoDB:', error.message);
     res.status(500).json({ error: 'Internal server error' });
   }
-  
 });
 
 app.listen(PORT, () => {
